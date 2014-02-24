7a2ab054a2f2ea28dd7251fe9b2f037ff2f6c72a
Nick Lobeck <nick@8am.ch>
2014-02-24 10:54:25 +0100

    Replaced Glyphicons with FontAwesome

--- includes/js/bootstrap.js
+++ includes/js/bootstrap.js
@@ -1,3 +1,3 @@
-/* @codekit-prepend "bootstrap/affix","bootstrap/alert","bootstrap/button","bootstrap/carousel","bootstrap/collapse","bootstrap/dropdown","bootstrap/modal","bootstrap/popover","bootstrap/scrollspy","bootstrap/tab","bootstrap/tooltip","bootstrap/transition","bootstrap/transition","bootstrap/wp";
+/* @codekit-prepend "bootstrap/affix","bootstrap/alert","bootstrap/button","bootstrap/carousel","bootstrap/collapse","bootstrap/dropdown","bootstrap/modal","bootstrap/tooltip","bootstrap/popover","bootstrap/scrollspy","bootstrap/tab","bootstrap/transition","bootstrap/transition","bootstrap/wp";
 
 */
